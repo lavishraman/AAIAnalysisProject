bf249d01b9bc23932d47ca87cfeba89bb46cac01
lavishraman <lavish5800@gmail.com>
2023-02-19 15:10:53 +0530

    db&admin

--- base/views.py
+++ base/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
-# Create your views here.
+from .models import Room 
+ # Create your views here.
 
 
-rooms = [
-    {'id':1 , 'name': 'Project 1'},
-    {'id':2 , 'name': 'Project 2'},
-    {'id':3 , 'name': 'Project 3'},
-
-] 
+# rooms = [
+#     {'id':1 , 'name': 'Project 1'},
+#     {'id':2 , 'name': 'Project 2'},
+#     {'id':3 , 'name': 'Project 3'},
+# ] 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room':room}
 
     return render(request,'base/room.html', context)
